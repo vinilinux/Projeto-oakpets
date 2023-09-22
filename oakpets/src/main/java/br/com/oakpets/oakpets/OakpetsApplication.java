2b5233f59359054a7f9635f1ad1fdd6ff366a251
vinilinux <vinicius.limalveslinux@gmail.com>
2023-09-21 21:05:23 -0300

    Criado projeto no spring e ajustado o readme do projeto

---
+++ oakpets/src/main/java/br/com/oakpets/oakpets/OakpetsApplication.java
@@ -0,0 +1,13 @@
+package br.com.oakpets.oakpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakpetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakpetsApplication.class, args);
+	}
+
+}
