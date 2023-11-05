e24fef5d11112b9d7ac6614d8c923bd78fd8aa85
Matheus Lima <37540388+Pidgeott@users.noreply.github.com>
2023-11-05 13:32:26 -0300

    feature/Landing Page
    - Inclusão da Landing Page no projeto, apenas front end

---
+++ oakpets/src/main/resources/static/JS/landing-page.js
@@ -0,0 +1,4 @@
+$(window).on("load", function() {
+    // Quando tudo, incluindo imagens, for carregado, seu código aqui será executado
+    $('#carouselExample').carousel(); // Inicializa o carrossel após tudo ser carregado
+});
