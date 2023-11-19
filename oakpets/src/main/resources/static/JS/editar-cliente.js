921c7f1b3fa308b25cff37a9ce200c624e4383c1
Vinicius Garcia <vinicius_souzagarcia@hotmail.com>
2023-11-19 01:46:26 -0300

    Finalizado HTML cadastro e edição cliente

--- oakpets/src/main/resources/static/JS/editar-cliente.js
+++ oakpets/src/main/resources/static/JS/editar-cliente.js
@@ -74,6 +74,28 @@ function salvarPerfil(event) {
         });
 }
 
+function updateUIBasedOnLoginStatus() {
+    const userName = localStorage.getItem("userName");
+    const userStatusElement = document.getElementById('userStatus');
+    const logoutLink = document.getElementById('logoutLink');
+
+    console.log("Valor de userName em localStorage:", localStorage.getItem("userName"));
+
+    if (userName) {
+        userStatusElement.classList.add('btnEntrar');
+        userStatusElement.innerHTML = `<i class="bi bi-person-fill"></i> <a href="minha-conta.html">${userName}</a>`;
+        logoutLink.style.display = 'inline';
+    } else {
+        userStatusElement.classList.remove('btnEntrar');
+        userStatusElement.innerHTML = `<a href="./login-cliente.html"><button class="btn btnEntrar text-left" type="submit"><i class="bi bi-person-fill"></i> <span>Entre ou <br> Cadastre-se</span></button></a>`;
+        logoutLink.style.display = 'none';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    updateUIBasedOnLoginStatus();
+});
+
 
 
 
