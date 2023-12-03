4781462b03d560013d60024e509f2b7888eb6abd
Matheus Lima <37540388+Pidgeott@users.noreply.github.com>
2023-12-03 18:30:17 -0300

    Ajustes Landing Page

--- oakpets/src/main/resources/static/JS/landing-page.js
+++ oakpets/src/main/resources/static/JS/landing-page.js
@@ -1,4 +1,54 @@
-$(window).on("load", function() {
-    // Quando tudo, incluindo imagens, for carregado, seu código aqui será executado
-    $('#carouselExample').carousel(); // Inicializa o carrossel após tudo ser carregado
+
+
+function recuperarInformacoesDoCarrinho() {
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+    return carrinho;
+}
+
+// Função para recuperar um item específico do carrinho com base no ID do produto
+function recuperarItemDoCarrinhoPorId(produtoId) {
+    const carrinho = recuperarInformacoesDoCarrinho();
+    const itemEncontrado = carrinho.find(item => item.produto.idProduct === produtoId);
+    return itemEncontrado || null;
+}
+
+function atualizarIconeCarrinhoGlobal() {
+    const carrinho = recuperarInformacoesDoCarrinho();
+    const quantidadeItens = carrinho.length;
+    const contadorCarrinho = document.getElementById("contador-carrinho");
+
+    console.log("Iniciando atualizarIconeCarrinhoGlobal");
+
+    if (contadorCarrinho) {
+        contadorCarrinho.textContent = quantidadeItens;
+        contadorCarrinho.style.display = quantidadeItens > 0 ? 'inline' : 'none';
+    }
+
+    console.log("AtualizarIconeCarrinhoGlobal concluído com sucesso");
+}
+
+function updateUIBasedOnLoginStatus() {
+    const userName = localStorage.getItem("userName");
+    const userStatusElement = document.getElementById('userStatus');
+    const logoutLink = document.getElementById('logoutLink');
+
+    console.log("Valor de userName em localStorage:", localStorage.getItem("userName"));
+
+    if (userName) {
+        userStatusElement.classList.add('btnEntrar');
+        userStatusElement.innerHTML = `<i class="bi bi-person-fill"></i> <a href="minha-conta.html">${userName}</a>`;
+        logoutLink.style.display = 'inline';
+    } else {
+        userStatusElement.classList.remove('btnEntrar');
+        userStatusElement.innerHTML = `<a href="./login-cliente.html"><button class="btn btnEntrar text-left" type="submit"><i class="bi bi-person-fill"></i> <span>Entre ou <br> Cadastre-se</span></button></a>`;
+        logoutLink.style.display = 'none';
+    }
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    atualizarIconeCarrinhoGlobal();
+    updateUIBasedOnLoginStatus();
 });
