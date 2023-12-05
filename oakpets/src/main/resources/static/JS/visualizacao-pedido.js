e59a7410c3108f3576008bd06a7a07cbdab8f13a
WilliamRibeiroQueiroz <williamrs.12@gmail.com>
2023-12-05 17:58:57 -0300

    Feito algumas alteracoes

--- oakpets/src/main/resources/static/JS/visualizacao-pedido.js
+++ oakpets/src/main/resources/static/JS/visualizacao-pedido.js
@@ -22,16 +22,9 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("AtualizarIconeCarrinhoGlobal concluído com sucesso");
     }
 
-
-
     atualizarIconeCarrinho();
 
 });
-
-
-
-
-
 
 $(document).ready(function () {
 
@@ -50,6 +43,8 @@ if (pedidoCompleto) {
 
     //Tratando o endereço
     var endereco = visualizacaoPedido.endereco;
+    endereco = endereco.replace("Endereço Entrega", "").trim();
+    console.log(endereco)
     var enderecoPedido = document.getElementById('endereco');
     enderecoPedido.textContent = endereco;
     var idEndereco = endereco.match(/\d+/);
@@ -90,13 +85,21 @@ if (carrinho) {
 
     if (produtosCarrinho.length > 0) {
         var listaProdutos = document.getElementById('listaProdutos');
+        var listaProdutosQuant = document.getElementById('ListaProdutosQuant');
+        var listaProdutosValor = document.getElementById(('ListaProdutosValor'));
 
         produtosCarrinho.forEach(function (produto) {
             var nomeProduto = produto.produto.name;
             var quantidadeProduto = produto.quantidade;
             var valorProduto = produto.total;
             var listItem = document.createElement('li');
-            listItem.textContent = nomeProduto + ' (Quantidade: ' + quantidadeProduto + ', Valor: R$ ' + valorProduto + ')';
+            var listItemQuant = document.createElement('li');
+            var listItemValor = document.createElement(('li'));
+            listItem.textContent = nomeProduto;
+            listItemQuant.textContent = quantidadeProduto;
+            listItemValor.textContent = valorProduto;
+            listaProdutosValor.appendChild(listItemValor);
+            listaProdutosQuant.appendChild((listItemQuant));
             listaProdutos.appendChild(listItem);
         });
     } else {
